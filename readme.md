868776cdf8d01297d2db83a091cb3a030e7c7a14
rahulgs12 <gsr1998@gmail.com>
2023-03-21 20:33:53 -0400

    add files

---
+++ readme.md
@@ -0,0 +1,56 @@
+# token-trekker-rs
+
+`token-trekker-rs` is a command-line tool for counting the total number of tokens in all files within a directory or matching a glob pattern, using various tokenizers.
+
+## Features
+
+- Supports multiple tokenizer options
+- Parallel processing for faster token counting
+- Outputs results in a colorized table
+
+## Installation
+
+To install `token-trekker-rs` from crates.io, run:
+
+```sh
+cargo install token-trekker-rs
+```
+
+## Building from Source
+
+To build token-trekker-rs from the source code, first clone the repository:
+
+```sh
+git clone https://github.com/1rgs/token-trekker-rs.git
+cd token-trekker-rs
+```
+
+Then build the project using cargo:
+
+```sh
+cargo build --release
+```
+
+The compiled binary will be available at ./target/release/token-trekker.
+
+## Usage
+
+To count tokens in a directory or for files matching a glob pattern, run the following command:
+
+```sh
+token-trekker --path <path_or_glob_pattern> --tokenizer <tokenizer>
+```
+
+Replace <path_or_glob_pattern> with the path to the directory or the glob pattern of the files to process, and <tokenizer> with one of the available tokenizer options:
+
+- p50k-base
+- p50k-edit
+- r50k-base
+- cl100k-base
+- gpt2
+
+For example:
+
+```sh
+token-trekker --path "path/to/files/*.txt" --tokenizer p50k-base
+```
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use anyhow::{Context, Result};
 use clap::{Parser, ValueEnum};
+use colored::Colorize;
 use glob::glob;
+use prettytable::{format, row, Table};
+use rayon::prelude::*;
 use std::fs::File;
 use std::io::prelude::*;
-use tiktoken_rs::{cl100k_base, p50k_base, p50k_edit, r50k_base};
+use tiktoken_rs::{cl100k_base, p50k_base, p50k_edit, r50k_base, CoreBPE};
+
+type TokenizerFunction = fn() -> Result<CoreBPE>;
 
 #[derive(ValueEnum, Clone, Copy, Debug)]
 enum Tokenizer {
@@ -11,12 +16,23 @@ enum Tokenizer {
     P50kBase,
     P50kEdit,
     R50kBase,
+    Gpt2,
 }
 
-/// Token Counter: Counts the total number of tokens in all files within a directory or matching a glob pattern.
+impl Tokenizer {
+    fn function(self) -> TokenizerFunction {
+        match self {
+            Tokenizer::Cl100kBase => cl100k_base,
+            Tokenizer::P50kBase => p50k_base,
+            Tokenizer::P50kEdit => p50k_edit,
+            Tokenizer::R50kBase => r50k_base,
+            Tokenizer::Gpt2 => r50k_base,
+        }
+    }
+}
+
 #[derive(Parser, Debug)]
 struct Args {
-    /// Path to directory or glob pattern
     #[clap(short, long)]
     path: String,
 
@@ -24,50 +40,68 @@ struct Args {
     tokenizer: Tokenizer,
 }
 
-fn count_tokens_in_file(file_path: &str, tokenizer: &Tokenizer) -> Result<usize> {
-    let mut file = File::open(file_path).context("Failed to open file")?;
-    let mut contents = String::new();
-    let file_read = file
-        .read_to_string(&mut contents)
-        .context("Failed to read file");
-
-    if file_read.is_err() {
-        eprintln!("{}", file_read.unwrap_err());
-
-        return Ok(0);
+fn count_tokens_in_file(file_path: &str, tokenizer_fn: TokenizerFunction) -> Option<usize> {
+    if std::path::Path::new(file_path).is_dir() {
+        return None;
     }
 
-    let tokens = match tokenizer {
-        Tokenizer::Cl100kBase => cl100k_base().unwrap().encode_with_special_tokens(&contents),
-        Tokenizer::P50kBase => p50k_base().unwrap().encode_with_special_tokens(&contents),
-        Tokenizer::P50kEdit => p50k_edit().unwrap().encode_with_special_tokens(&contents),
-        Tokenizer::R50kBase => r50k_base().unwrap().encode_with_special_tokens(&contents),
-    };
+    let file = File::open(file_path);
+    if file.is_err() {
+        eprintln!("Failed to open file: {}", file_path);
+        return None;
+    }
+    let mut contents = String::new();
+    let file_read = file
+        .unwrap()
+        .read_to_string(&mut contents)
+        .context("Failed to read file contents");
 
-    println!("{}: {}", file_path, tokens.len());
+    if file_read.is_err() {
+        eprintln!("Failed to read file: {}", file_path);
+        return None;
+    }
 
-    Ok(tokens.len())
+    let tokenizer = tokenizer_fn().unwrap();
+    let tokens = tokenizer.encode_with_special_tokens(&contents);
+
+    Some(tokens.len())
 }
-
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    let mut total_tokens = 0;
+    let entries: Result<Vec<_>, _> = glob(&args.path)
+        .context("Failed to read glob pattern")?
+        .collect();
+    let entries = entries?;
 
-    for entry in glob(&args.path).context("Failed to read glob pattern")? {
-        match entry {
-            Ok(path) => {
-                let file_path = path.to_str().unwrap();
-                println!("{}", file_path);
+    let tokenizer_fn = args.tokenizer.function();
 
-                let tokens = count_tokens_in_file(file_path, &args.tokenizer)?;
-                total_tokens += tokens;
-            }
-            Err(e) => eprintln!("{:?}", e),
-        }
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+    table.add_row(row!["Filename".cyan(), "Tokens".cyan()]);
+
+    let vec = entries
+        .par_iter()
+        .filter_map(|entry| {
+            let entry = entry.to_str().unwrap();
+            let tokens = count_tokens_in_file(entry, tokenizer_fn)?;
+            return Some((entry, tokens));
+        })
+        .collect::<Vec<_>>();
+
+    for (filename, tokens) in &vec {
+        table.add_row(row![filename, tokens.to_string().bold().cyan()]);
     }
 
-    println!("Total tokens: {}", total_tokens);
+    let total = vec.iter().fold(0, |acc, (_, tokens)| acc + tokens);
+
+    table.printstd();
+
+    println!(
+        "{}: {}",
+        "Total tokens".bold(),
+        total.to_string().bold().cyan()
+    );
 
     Ok(())
 }
